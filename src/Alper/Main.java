a2e4e85c5ce211b5b29fa0ca1b1ab1f992e280dc
Alper Eş <alperes13@hotmail.com>
2022-07-06 22:32:04 +0300

    asdasdasdasd

--- src/Alper/Main.java
+++ src/Alper/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Alper");
         System.out.println("deneme 1");
+        System.out.println();
     }
 }
